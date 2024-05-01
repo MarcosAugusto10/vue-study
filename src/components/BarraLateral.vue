ed2d42511f82f4ac9470fbb515648961d2187188
MarcosAugusto10 <48873276+MarcosAugusto10@users.noreply.github.com>
2024-05-01 16:21:49 -0300

    Incluindo barralateral e tarefalComponent

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>
+          <img src="../assets/logo.png" alt="">  
+        </h1>
+        
+    </header>
+  </template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent(
+    {
+        name:'BarraLateral'
+    })
+</script>
+
+<style scoped>
+header{
+    background-color: #5a0e99;
+    width: 100%;
+    height: 100vh;
+}
+@media only screen and (max-width: 1000px) {
+    header{
+        padding: 2.5rem;
+        height:  auto;
+    }
+}
+</style>
+
---
+++ src/components/TarefaComponent.vue
@@ -0,0 +1,13 @@
+<template>
+<h1 > Teste de Vue</h1>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'TarefaComponent'
+})
+</script>
+
+<style scoped>
+
+</style>
