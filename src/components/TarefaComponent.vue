53d95a94a278ef3565b56367b6566c8452ae8c19
MarcosAugusto10 <48873276+MarcosAugusto10@users.noreply.github.com>
2024-06-11 22:53:55 -0300

    Adicionando controles aos componentes

--- src/components/TarefaComponent.vue
+++ src/components/TarefaComponent.vue
@@ -1,47 +1,17 @@
 <template>
 <body>
-    <div class="section" >
-        <!-- Botão Play -->
-        
-        <input type="text" id="nome" name="nome" placeholder="Digite a tarefa que você quer iniciar" class="label">
-        <!--<p id="timer-display" class="subtitle timer">00:00:00</p>-->
-        <button class="button is-success botao">
-            <span class="icon is-small">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>Play</span>
-        </button>
-
-        <!-- Botão Stop -->
-        <button class="button is-danger botao">
-            <span class="icon is-small">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>Stop</span>
-        </button>
-    </div>
+    <TemporizadorComponent/>
 </body>
 </template>
 <script lang='ts'>
 import { defineComponent } from 'vue';
+import TemporizadorComponent from './TemporizadorComponent.vue';
 export default defineComponent({
-    name: 'TarefaComponent'
+    name: 'TarefaComponent',
+    components: { TemporizadorComponent }
 })
 </script>
 
 <style scoped>
-.botao 
-{
-    float: right;
-}
-.label
-{
-    width: 600px;
-    height: 20px;
-    float: left;
-}
-.timer
-{
-font-size: 10px;
-}
+
 </style>
---
+++ src/components/TemporizadorComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <body>
+        <div class="section" >
+        <!-- Botão Play -->
+        
+        <input type="text" id="nome" name="nome" placeholder="Digite a tarefa que você quer iniciar" class="label">
+        <!--<p id="timer-display" class="subtitle timer">00:00:00</p>-->
+        <button class="button is-success botao">
+            <span class="icon is-small">
+                <i class="fas fa-play"></i>
+            </span>
+            <span>Play</span>
+        </button>
+
+        <!-- Botão Stop -->
+        <button class="button is-danger botao">
+            <span class="icon is-small">
+                <i class="fas fa-stop"></i>
+            </span>
+            <span>Stop</span>
+        </button>
+    </div>
+    </body>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent(
+    {
+        name:"TemporizadorComponent"
+    })
+</script>
+<style scoped>
+.botao 
+{
+    float: right;
+}
+.label
+{
+    width: 600px;
+    height: 20px;
+    float: left;
+}
+.timer
+{
+font-size: 10px;
+}
+</style>
